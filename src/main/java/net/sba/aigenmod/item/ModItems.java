acdc266e01573d7455fc83ed7536585b953e59f8
DevSBA <mansur.tallyn@gmail.com>
2025-06-12 21:21:36 -0400

    yes

--- src/main/java/net/sba/aigenmod/item/ModItems.java
+++ src/main/java/net/sba/aigenmod/item/ModItems.java
@@ -1,16 +1,17 @@
 package net.sba.aigenmod.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.sba.aigenmod.AiGeneratedMobs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item GREEN_STICK = registerItem("green_stick", new Item(new Item.Settings()));
+    public static final Item GREEN_STICK =registerItem("green_stick",new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(AiGeneratedMobs.MOD_ID,"green_stick")))));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(AiGeneratedMobs.MOD_ID, name), item);
