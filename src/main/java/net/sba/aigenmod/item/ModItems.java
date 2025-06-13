807e7665d11a6ccfec0f87461ddf3bcb0f477d6a
DevSBA <mansur.tallyn@gmail.com>
2025-06-12 20:11:20 -0400

    added everything

--- src/main/java/net/sba/aigenmod/item/ModItems.java
+++ src/main/java/net/sba/aigenmod/item/ModItems.java
@@ -12,7 +12,6 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item GREEN_STICK = registerItem("green_stick", new Item(new Item.Settings()));
 
-
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(AiGeneratedMobs.MOD_ID, name), item);
     }
